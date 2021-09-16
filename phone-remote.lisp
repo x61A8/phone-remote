43d64ac8134a35e72e678488eb5eb651ce0a1989
x61A8 <x61A8@protonmail.com>
2021-09-15 20:35:33 -0400

    Add char->virtual-keycode map

--- phone-remote.lisp
+++ phone-remote.lisp
@@ -1,1 +1,18 @@
 (in-package #:phone-remote)
+
+;;; Keyboard Input
+(defun init-vks ()
+  (let ((ret (make-hash-table :size 36)))
+    (loop for char from (char-code #\0) to (char-code #\9)
+	  for vk from #x30 to #x39
+	  do (setf (gethash (code-char char) ret) vk))
+    (loop for char from (char-code #\A) to (char-code #\Z)
+	  for vk from #x41 to #x5A
+	  do (setf (gethash (code-char char) ret) vk))
+    ret))
+
+(defparameter *vks* (init-vks))
+(defun get-vk (char)
+  (gethash char *vks*))
+
+
