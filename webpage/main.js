6654b2280447b78e5b067bfbc3fa29ad02b67629
x61A8 <x61A8@protonmail.com>
2021-10-20 17:50:07 -0400

    Aggregate declarations into getWS function

--- webpage/main.js
+++ webpage/main.js
@@ -1,3 +1,9 @@
-const url = new URL(window.location.href);
-const address = url.hostname;
-const wsPort = url.searchParams.get("ws");
+function getWS () {
+    const url = new URL(window.location.href);
+    const address = url.hostname;
+    const wsPort = url.searchParams.get("ws");
+    const wsUrl = "ws://" + address + ":" + wsPort + "/";
+    return new WebSocket(wsUrl);
+}
+const socket = getWS();
+let player = 1;
