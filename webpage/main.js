bc016e319c1c29b1013335bd01ff53044251db04
x61A8 <x61A8@protonmail.com>
2021-10-22 15:26:31 -0400

    Consistently use single quote in main.js

--- webpage/main.js
+++ webpage/main.js
@@ -2,15 +2,15 @@
 function getWS () {
     const url = new URL(window.location.href);
     const address = url.hostname;
-    const wsPort = url.searchParams.get("ws");
-    const wsUrl = "ws://" + address + ":" + wsPort + "/";
+    const wsPort = url.searchParams.get('ws');
+    const wsUrl = 'ws://' + address + ':' + wsPort + '/';
     return new WebSocket(wsUrl);
 }
 const socket = getWS();
 
 // Player Switching
 let player = 1;
-const playerButton = document.getElementById("player");
+const playerButton = document.getElementById('player');
 function switchPlayer () {
     if (player === 1) {
 	player = 2;
